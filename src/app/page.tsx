71bea783d9181ff4111cf1b6a7b42ef8db5865ef
cruz-welandi <cruzwelandi@gmail.com>
2024-09-20 13:09:35 +0100

    delete test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import HeadTitle from "@/components/headTitle";
+
 
 
 export default function Home() {
   return (
-    <div className="h-[500px] w-[500px] bg-foreground">
-      <HeadTitle label={"Dashboad"}/>
+    <div>
+      
     </div> 
   );
 }
