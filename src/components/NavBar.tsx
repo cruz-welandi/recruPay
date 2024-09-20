95a11c03d9f5c0983d631831f75c5e43e25e7cca
JojoELITE <jojoelite1014@gmail.com>
2024-09-20 12:11:23 +0100

    I have just added the button which will be used to add a new job offer from the navBar component.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import ButtonAdd from './navbar/ButtonAdd'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+      <ButtonAdd/>
+    </div>
   )
 }
---
+++ src/components/navbar/ButtonAdd.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function ButtonAdd() {
+  return (
+    <div>
+        <Button className='rounded-full h-20 w-20 bg-yellow-500 text-black text-3xl font-light'>
+          +
+        </Button>
+    </div>
+  )
+}
