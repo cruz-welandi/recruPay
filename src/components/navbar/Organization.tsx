2c29c3917b1d5bd554f0554ba0ceecc06c9a5231
JojoELITE <jojoelite1014@gmail.com>
2024-09-20 16:49:14 +0100

    je travaille actuellement sur le Modal.

--- src/components/navbar/Organization.tsx
+++ src/components/navbar/Organization.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const organizations = [
     {
+        logo: "./logo/logo01.png",
         name: "Samba Tech",
         offre: '2 offres en cours'
     },
     {
+        logo: "",
         name: "Ogooue Technologie",
         offre: '2 offres en cours'
     },
@@ -14,13 +16,21 @@ const organizations = [
 export default function Organization() {
     return (
         <div>
-            <div>
+            <div className='flex flex-col gap-y-2'>
                 {organizations.map((item, index) => (
-                    <div key={index} className="items-center py-2">
+                    <div key={index} className="flex items-center space-x-3 py-2 bg-[#f3f1f2] ">
+
+                        {item.logo && (
+                            <img 
+                                src={item.logo} 
+                                alt={`${item.name} logo`} 
+                                className="h-10 w-10 object-contain" 
+                            />
+                        )}
                         <div>
                             <span className="font-bold">{item.name}</span>
+                            <p className="text-gray-500">{item.offre}</p>
                         </div>
-                        <p className="text-gray-500 ">{item.offre}</p>
                     </div>
                 ))}
             </div>
