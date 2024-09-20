1a80be090258a8288b6c15d938252a4292879dd6
JojoELITE <jojoelite1014@gmail.com>
2024-09-20 12:18:30 +0100

    I have just added the button which will be used to display notifications from the navBar component.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ButtonAdd from './navbar/ButtonAdd'
+import NotificationButton from './navbar/NotificationButton'
 
 export default function NavBar() {
   return (
     <div>
       <ButtonAdd/>
+      <NotificationButton />
     </div>
   )
 }
