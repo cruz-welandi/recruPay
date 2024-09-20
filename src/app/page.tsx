17874a4e6632dc0df94f444f9999f5d46bd2fa70
elieboulingui <elieboulingui2@gmail.com>
2024-09-20 01:28:54 +0100

    jo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import HeadTitle from "@/components/headTitle";
 export default function Home() {
   return (
     <div className="h-[500px] w-[500px] bg-foreground">
-      <HeadTitle label={"Dashboad"}/>
+
+      <HeadTitle label={"Dashboad "}/>
+      
     </div> 
   );
 }
