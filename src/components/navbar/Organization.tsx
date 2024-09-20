76dae4a539e20ed6fb91193783811c8115fb2f83
JojoELITE <jojoelite1014@gmail.com>
2024-09-20 17:26:30 +0100

    I am currently working on the NavBar Modal.

--- src/components/navbar/Organization.tsx
+++ src/components/navbar/Organization.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { FiEdit2, FiRefreshCw } from 'react-icons/fi'; 
 
 const organizations = [
     {
         logo: "./logo/logo01.png",
         name: "Samba Tech",
-        offre: '2 offres en cours'
+        offre: '2 offres en cours',
+        iconTransfert: <FiRefreshCw className="h-5 w-5 text-gray-500" />,
+        iconEdit: <FiEdit2 className="h-5 w-5 text-gray-500" />
     },
     {
-        logo: "",
+        logo: "./logo/logo01.png",
         name: "Ogooue Technologie",
-        offre: '2 offres en cours'
+        offre: '2 offres en cours',
+        iconTransfert: <FiRefreshCw className="h-5 w-5 text-gray-500" />,
+        iconEdit: <FiEdit2 className="h-5 w-5 text-gray-500" />
     },
 ];
 
@@ -18,8 +23,9 @@ export default function Organization() {
         <div>
             <div className='flex flex-col gap-y-2'>
                 {organizations.map((item, index) => (
-                    <div key={index} className="flex items-center space-x-3 py-2 bg-[#f3f1f2] ">
+                    <div key={index} className="flex items-center space-x-3 py-2 bg-[#f3f1f2] p-2 rounded-md">
 
+                        {/* Logo de l'organisation */}
                         {item.logo && (
                             <img 
                                 src={item.logo} 
@@ -27,9 +33,25 @@ export default function Organization() {
                                 className="h-10 w-10 object-contain" 
                             />
                         )}
-                        <div>
+
+                        {/* Détails de l'organisation */}
+                        <div className="flex-grow">
                             <span className="font-bold">{item.name}</span>
                             <p className="text-gray-500">{item.offre}</p>
+                        </div>
+
+                        {/* Icônes d'action */}
+                        <div className="flex space-x-2 items-center">
+                            {item.iconTransfert && (
+                                <span className="cursor-pointer hover:text-blue-500 text-white bg-[#fa9938] rounded-full">
+                                    {item.iconTransfert}
+                                </span>
+                            )}
+                            {item.iconEdit && (
+                                <span className="cursor-pointer hover:text-green-500 text-white bg-[#0c2c2b]  rounded-full">
+                                    {item.iconEdit}
+                                </span>
+                            )}
                         </div>
                     </div>
                 ))}
