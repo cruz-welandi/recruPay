747c25d90c8d710aad9674f0028a8e3dc898ccf4
elieboulingui <elieboulingui2@gmail.com>
2024-09-23 11:09:55 +0100

    i add a redirctor which allows us to navigate into a other page

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -5,23 +5,19 @@ import React from 'react';
 type SideBarProps = {
     label?: string;
     size?: number;
-    Icon: React.ElementType;
+    Icon?: React.ElementType;
     link: string;
     onclick: (link :string)=> void;
     selected:boolean;
-
 };
-
 const Link: React.FC<SideBarProps> = ({ label, size, Icon, link ,selected,onclick}) => {
     const isExpanded = useSideBarStore((state) => state.expanded);
-
-
     return (
         <div
             onClick={()=>onclick(link)}
             className={`flex gap-4 text-white text-md rounded-sm  py-1.5 px-4 ${selected && !isExpanded ? 'bg-theme-2' : ''}`}
         >
-            <Icon size={19} className= {`${isExpanded && selected && 'text-theme-2'}`} />
+             {Icon && <Icon size={19} className= {`${isExpanded && selected && 'text-theme-2'}`} />}
             <span style={{ fontSize: size ? size : 8, display: isExpanded? 'none': 'block'}}>
                 {label}
             </span>
